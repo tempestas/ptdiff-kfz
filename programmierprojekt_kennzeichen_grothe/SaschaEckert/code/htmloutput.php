0464eb81f7bdd324cbbbde7c0a90b953f0b1199b
unknown <foo@bla.de>
2014-05-14 09:43:14 +0200

    asdfghj

--- programmierprojekt_kennzeichen_grothe/SaschaEckert/code/htmloutput.php
+++ programmierprojekt_kennzeichen_grothe/SaschaEckert/code/htmloutput.php
@@ -7,29 +7,29 @@ class HTMLOutput
     function renderSuggestionsSection($locationIDs)
     {
         $html = "";
-        
+
         $html = "<table id='list' onclick=\"SelectCountry()\">\n";
         $html = $html."<thead><tr><th>Kennzeichen</th><th>Landkreis</th><th>Kreisstadt</th><th>Bundesland</th></tr></thead><tbody>\n";
         foreach($locationIDs as $l)
         {
             $html = $html."<tr>";
-            $html = $html."<td id='kurz'>".$l["kreis_kurz"]."</td>"."<td>".$l["kreis_name"]."</td>"."<td>".$l["kreis_stadt"]."</td>"."<td>".$l["bundesland"]."</td>";
+            $html = $html."<td id='kurz'><input type='button' id='kfz_button' name='Kreisname' value='".$l["kreis_kurz"]."'/></td>"."<td>".$l["kreis_name"]."</td>"."<td>".$l["kreis_stadt"]."</td>"."<td>".$l["bundesland"]."</td>";
             $html = $html."</tr>\n";
         }
         $html = $html."</tbody></table>\n";
         return $html;
     }
-    
+
     function renderInfoSection($data)
     {
         $html = "";
-        $html = "<table>\n";        
-        
+        $html = "<table>\n";
+
         $html = $html."<tr><td>Kennzeichen:</td>"."<td>".$data["kreis_kurz"]."</td></tr>\n";
         $html = $html."<tr><td>Landkreis:</td>"."<td>".$data["kreis_name"]."</td></tr>\n";
         $html = $html."<tr><td>Kreisstadt:</td>"."<td>".$data["kreis_stadt"]."</td></tr>\n";
         $html = $html."<tr><td>Bundesland:</td>"."<td>".$data["bundesland"]."</td></tr>\n";
-        
+
         $html = $html."</table>\n";
         return $html;
     }
@@ -39,15 +39,15 @@ class HTMLOutput
         $html = "<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.de/maps?q=$locationID&amp;output=embed'></iframe>";
         return $html;
     }
-    
+
     function renderWikiEmbedding($locationID)
     {
         $data = array();
         $data = file_get_contents("http://de.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=".$locationID."&exintro=1");
         $data = get_object_vars(json_decode($data));
-		$data = $data['query']->pages;
-		$key=key(get_object_vars($data));
-		$data=$data->$key->extract;
+                $data = $data['query']->pages;
+                $key=key(get_object_vars($data));
+                $data=$data->$key->extract;
 
 var_export($data);
         return $data["extract"];
